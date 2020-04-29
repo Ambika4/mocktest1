99434ed710dd9056b7733db73da131e5c10ebc5d
Ambika Kumari <kambika1309@gmail.com>
2020-04-29 17:12:11 +0530

    added body parser

--- index.js
+++ index.js
@@ -1,10 +1,13 @@
 const express = require('express');
 const db=require('./config/mongoose');
+const bodyParser = require('body-parser')
 const port=8000;/** On port 80 all website hosts */
 /*app listen to the port*/
 
 
 const app = express();
+app.use(bodyParser.json());
+app.use(express.urlencoded());
 
 app.get('/', (req, res) => res.send('Hello World!'));
 
